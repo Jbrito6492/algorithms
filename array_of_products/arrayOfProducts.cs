7534958071c4a8ac29209b7bf988ed226d7b6535
Josh Brito <jbrito6492@gmail.com>
2021-02-23 18:34:34 -0700

    added optimal solution

--- array_of_products/arrayOfProducts.cs
+++ array_of_products/arrayOfProducts.cs
@@ -2,7 +2,17 @@ using System;
 
 public class Program
 {
+    // brute force method
+    // time complexity O(n2)
     public int[] ArrayOfProducts(int[] array)
     {
+        int[] leftProducts = new int[array.Length];
+        int[] rightProducts = new int[array.Length];
+        int[] products = new int[array.Length];
+
+        int leftRunningProduct = 1;
+        for (int i = 0; i < array.Length; i++)
+        {
+        }
     }
 }
