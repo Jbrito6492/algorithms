2bb9799183fc77962577b01ea7a904b85ec6e35b
Josh Brito <jbrito6492@gmail.com>
2021-02-23 18:35:15 -0700

    added optimal solution

--- array_of_products/arrayOfProducts.cs
+++ array_of_products/arrayOfProducts.cs
@@ -13,6 +13,19 @@ public class Program
         int leftRunningProduct = 1;
         for (int i = 0; i < array.Length; i++)
         {
+            leftProducts[i] = leftRunningProduct;
+            leftRunningProduct *= array[i];
         }
+        int rightRunningProduct = 1;
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            rightProducts[i] = rightRunningProduct;
+            rightRunningProduct *= array[i];
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            products[i] = leftProducts[i] * rightProducts[i];
+        }
+        return products;
     }
 }
